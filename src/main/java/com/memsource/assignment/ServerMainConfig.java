b61ad4e549c41b9c53193f614b89e191e0585dce
Tomas Fechtner <fechtnertomas@gmail.com>
2018-01-14 18:56:25 +0100

    Initial commit

---
+++ src/main/java/com/memsource/assignment/ServerMainConfig.java
@@ -0,0 +1,17 @@
+package com.memsource.assignment;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class ServerMainConfig {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(ServerMainConfig.class, args);
+    }
+}
